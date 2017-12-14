ac68d79164097c3d6e1042bb3eacffe4bf67c8ba
Joe Beaver <joseph.beaver@gmail.com>
2017-12-14 22:25:13 +0000

    Added docstrings and PEP8-ified

--- PointKineticsModel.py
+++ PointKineticsModel.py
@@ -1,26 +1,49 @@
 class PointKineticsModel:
-    
+    """Class for the kinetics model."""
+
     def __init__(self, constants):
-        
+
         self.constants = constants
-    
-    def dBydt(self, vector):
-        
-        n = vector[1]
+
+    def d_by_dt(self, vector):
+        """Calculates rate of change of precursors.
+
+        Arguments:
+            vector - list containing the initial value of the neutron
+            population, the reactivity change made to the system, and the
+            precursor values.
+
+        Returns:
+            dt_dt - the rate of change of time over time.
+            dn_dt - the rate of change of population over time.
+            drho_dt - rate of change of reactivity over time.
+            dPrecursor_dt - amount contributed by the neutron energy group
+            precursors to each of the above.
+
+        Excepts:
+            None"""
+
+        neutron_population = vector[1]
         rho = vector[2]
         precursors = vector[3:]
-        
-        dtdt = 1.0
-        
-        dndt = (rho-self.constants.beta)*n/self.constants.nGenTime
+
+        dt_dt = 1.0
+
+        dn_dt = ((rho-self.constants.beta) *
+                 neutron_population) / self.constants.nGenTime
+
         for i in range(self.constants.ndg):
-            dndt +=self.constants.lambdai[i]*precursors[i]
-        
-        drhodt = 0.0
-        
-        dPrecdt = [0.0]*self.constants.ndg
-        
+            dn_dt += self.constants.lambdai[i] * precursors[i]
+
+        drho_dt = 0.0
+
+        dPrecursor_dt = [0.0] * self.constants.ndg
+
         for i in range(self.constants.ndg):
-            dPrecdt[i]=(self.constants.betai[i]*n/self.constants.nGenTime)-self.constants.lambdai[i]*precursors[i]
-        
-        return [dtdt, dndt, drhodt]+dPrecdt
+            dPrecursor_dt[i] = ((self.constants.betai[i]
+                                 * neutron_population)
+                                / self.constants.nGenTime)\
+                    - (self.constants.lambdai[i]
+                       * precursors[i])
+
+        return [dt_dt, dn_dt, drho_dt] + dPrecursor_dt
--- point_kinetics.py
+++ point_kinetics.py
@@ -1,26 +1,28 @@
+"""Example use of the PointKineticsSolver"""
 #!/usr/bin/python
 
-from PointKineticsSolver import *
+from PointKineticsSolver import PointKineticsSolver
+
 
 core1 = PointKineticsSolver()
 
+# Set starting neutrons
 core1.set_neutrons(1E8)
 
-# Run for 60 seconds to reach an initial equilibrium
+# Run for 60 seconds to reach initial equilibrium
 core1.solve(t_stop=60, log_freq=0.1)
 
 # Positive reactivity step, run for further 60 seconds
 core1.set_rho(1E-3)
 core1.solve(t_stop=120, log_freq=0.1)
 
-# Negative reactivity step, run for further 60 seconds 
+# Negative reactivity step, run for further 60 seconds
 core1.set_rho(-1E-3)
 core1.solve(t_stop=180, log_freq=0.1)
 
-# Back to zero critical, run for further 60 seconds
+# Back to zero reactivity (critical), run for further 60 seconds
 core1.set_rho(0.0)
 core1.solve(t_stop=240, log_freq=0.1)
-
 
 core1.plot_rho()
 core1.plot_neutrons()
