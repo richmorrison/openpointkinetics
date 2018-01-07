2ce525984bee3ef0e37d1a01f190571329226f5e
Rich Morrison <rich@richmorrison.com>
2018-01-07 16:39:27 +0000

    Added thermal parameters to PointKineticsState.py

--- PointKineticsState.py
+++ PointKineticsState.py
@@ -6,15 +6,45 @@ class PointKineticsState:
 
     def __init__(self, ndg):
 
+        # Number of delayed groups
+        #
         self.ndg = ndg
+        
+        # Core power
+        #
         self.p = 0.0
+        
+        # Core reactivity
+        #
         self.rho = 0.0
-
+        
+        # Temperature of the core (isothermal approximation)
+        #
+        self.temperature = 0.0
+        
+        # Steam demand on the core (Watts)
+        #
+        self.demand = 0.0
+        
+        # alphaT could vary with time or state, so is not considered a constant
+        # and is best placed in this module
+        # (isothermal approximation)
+        #
+        self.alphaT = 0.0
+        
+        # List of precursor populations in each group
+        # Since we are modelling power and not neutron numbers,
+        # "precursor population"" is to be interpreted as:
+        #
+        # energyPerFission*fissionCrossSection*actualPrecursorPopulation
+        # 
         self.precursors = [0.0]*ndg
 
-        self.t = 0.0
+        # Time stamp of this state
+        self.time = 0.0
 
-        self.vectorLen = self.ndg+3
+        # Length of this state when vectorised
+        self.vectorLen = self.ndg+6
 
     def vectorise(self):
         """Pack the separate paramers into a single list.
@@ -31,10 +61,13 @@ class PointKineticsState:
 
         vector = [0.0]*self.vectorLen
 
-        vector[0] = self.t
+        vector[0] = self.time
         vector[1] = self.p
         vector[2] = self.rho
-        vector[3:] = self.precursors
+        vector[3] = self.temperature
+        vector[4] = self.demand
+        vector[5] = self.alphaT
+        vector[6:] = self.precursors
 
         return vector
 
@@ -45,14 +78,17 @@ class PointKineticsState:
             print("Wrong vector length for vector load operation.")
             exit()
 
-        self.t = vector[0]
+        self.time = vector[0]
         self.p = vector[1]
         self.rho = vector[2]
-        self.precursors = vector[3:]
+        self.temperature = vector[3]
+        self.demand = vector[4]
+        self.alphaT = vector[5]
+        self.precursors = vector[6:]
 
         return None
 
     def get_t(self):
         """Return current time value."""
 
-        return self.t
+        return self.time
