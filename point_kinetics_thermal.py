fb1f6451a16b696dcd28538c810a8e3bcbb45336
Rich Morrison <rich@richmorrison.com>
2018-01-07 22:06:03 +0000

    Further developed the thermal feedback demo.

--- point_kinetics_thermal.py
+++ point_kinetics_thermal.py
@@ -21,7 +21,7 @@ core1.set_temperature(300.0)
 core1.set_heatCapacity(334.5*1E6*4.1813)
 
 # Not sure what a representative alpha-T would be. Picking -2.5E-4 as
-# a right-order-of-magnitude value.
+# a rough right-order-of-magnitude value.
 core1.set_alphaT(-2.5E-4)
 
 # Set the core demand to the steam power of 3500 MW
@@ -30,11 +30,29 @@ core1.set_demand(3500.0E6)
 # Set the core power to the same as the steam power of 3500 MW)
 core1.set_power(3500.0E6)
 
-# Set initial reactivity.
+# Set initial reactivity (Only do set_rho() once or things get confused)
 core1.set_rho(0.0)
 
-# Half-hour simulated time
-core1.solve(t_change=1800, log_freq=0.1)
+# 5 mins to settle
+core1.solve(t_change=300, log_freq=0.1)
+
+# Let's suppose we raise the control rods and inject some reactivity without
+# changing the steam demand. We should see a self correcting power
+# transient, self-limited by a change in temperature. This means that
+# operationally the rods can be used to control core temperature, rather
+# than power.
+core1.add_rho(1E-3)
+core1.solve(t_change=300, log_freq=0.1) # Give it 5 minutes to watch equilibrium evolve.
+
+# Back to where we were.
+core1.add_rho(-1E-3)
+core1.solve(t_change=300, log_freq=0.1)
+
+# Instead, core power is controlled by directly taking more steam from
+# the plant. The resulting temperature change should cause a power increase
+# and we should return to the standard operating temperature.
+core1.set_demand(4000.0E6)
+core1.solve(t_change=300, log_freq=0.1)
 
 core1.plot_rho()
 core1.plot_power()
